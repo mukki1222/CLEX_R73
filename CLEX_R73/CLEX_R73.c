1dfe6fa0abda8aab2d536a31f0d17ac601cf676f
mukki1222 <mukki1222@gmail.com>
2013-01-09 09:10:22 +0900

    応用プログラミングAの課題1

---
+++ CLEX_R73/CLEX_R73.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main(void){
+	int a=10,b=3,c=-5;
+	printf("aとbの論理積:a&b=%d\n",a&b);
+	printf("aとbの論理和:a|b=%d\n",a|b);
+	printf("aとbの排他的論理和=:a^b=%d\n",a^b);
+	printf("aのビット反転:~a=%d\n",~a);
+	printf("bのビット反転:~b=%d\n",~b);
+	printf("cのビット反転:~c=%d\n",~c);
+	printf("aを左に1ビットシフト:a<<1=%d\n",a<<1);
+	printf("bを左に1ビットシフト:b<<1=%d\n",b<<1);
+	printf("cを左に1ビットシフト:c<<1=%d\n",c<<1);
+	printf("aを右に1ビットシフト:a>>1=%d\n",a>>1);
+	printf("bを右に1ビットシフト:b>>1=%d\n",b>>1);
+	printf("cを右に1ビットシフト:c>>1=%d\n",c>>1);
+}
